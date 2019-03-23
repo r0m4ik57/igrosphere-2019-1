8163ebf873c07b162acee0650102f5dace93fc35
Roman.Naryshkin <bitstop57@gmail.com>
2019-03-23 03:05:50 +0300

    Script fix

--- parser.py
+++ parser.py
@@ -3,16 +3,10 @@ import re
 
 def read_file(file_path):
     with open(file_path, 'r')as f:
-        return f.read()
+        return f.readlines()
 
 
 def filter_lines(regexp, lines):
-    result = []
-    for line in lines:
-        if re.match(regexp, line):
-            result.append(line)
-        else:
-            print(f"line {line} don't match")
     return [line for line in lines if re.match(regexp, line)]
 
 
@@ -20,6 +14,7 @@ if __name__ == "__main__":
     filename = "coverage-error.log"
     regexp = r"\[\d\d\d\d\.\d\d\.\d\d \d\d:\d\d:\d\d\].+"
     lines = read_file(filename)
+    filteredLines = filter_lines(regexp, lines)
     print("Author is roman.naryshkin")
-    print(filter_lines(regexp, lines))
-
+    for line in filteredLines:
+        print(line)
